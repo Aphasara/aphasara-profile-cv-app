077d0cf4ca543577b57fbda503be71407c62ad4e
Aphasara <s6419410038@sau.ac.th>
2025-04-01 16:42:56 +0700

    first2

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
 import React from 'react'
-import {CssBaseline} from '@mui/material'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Profile from './views/profile'
+import { CssBaseline } from '@mui/material'
+import Profile from './views/Profile'
 
-export default function () {
+function App() {
   return (
     <>
-    <CssBaseline />
-    <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Profile/>}/>
-    </Routes>
-    </BrowserRouter>
+      <CssBaseline />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Profile />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
+
+export default App
